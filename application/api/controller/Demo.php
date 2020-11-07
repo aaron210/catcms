481e56432909dcd2eda835c783f27b2cb47b0a1a
Simon <978165754@qq.com>
2020-11-07 08:12:09 +0800

    初始化

--- application/api/controller/Demo.php
+++ application/api/controller/Demo.php
@@ -9,8 +9,6 @@ use app\common\controller\Api;
  */
 class Demo extends Api
 {
-    // ceshi
-
     //如果$noNeedLogin为空表示所有接口都需要登录才能请求
     //如果$noNeedRight为空表示所有接口都需要验证权限才能请求
     //如果接口已经设置无需登录,那也就无需鉴权了
